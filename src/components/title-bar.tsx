b1256fe7991936c9ec976fdcdab80693aa21dc94
Jp Sia <johnpaulsia4@gmail.com>
2023-05-10 18:31:19 +0800

    feat: add title-bar component

---
+++ src/components/title-bar.tsx
@@ -0,0 +1,12 @@
+function TitleBar() {
+  return (
+    <div className='title-bar'>
+      <div className='title-bar-text'>MS Paint</div>
+      <div className='title-bar-controls'>
+        <button aria-label='Close' />
+      </div>
+    </div>
+  )
+}
+
+export default TitleBar
