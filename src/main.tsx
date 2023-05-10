1402c36f5de1969dcbecaaf219ddff78ef76b4ca
Jp Sia <johnpaulsia4@gmail.com>
2023-05-10 18:34:06 +0800

    chore: fix app component import

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import App from '@/app'
 import { store } from '@/redux/store'
+import App from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
