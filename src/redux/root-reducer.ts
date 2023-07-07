a1f3ab22380b0b7e7d828fe08b1c5925e37d8ab9
Jp Sia <johnpaulsia4@gmail.com>
2023-07-07 14:09:17 +0800

    chore: update reducer

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,16 +1,23 @@
-import { Reducer } from 'redux'
-
-import { Action, FINISH_STROKE, START_STROKE, UPDATE_STROKE } from './actions'
+import {
+  Action,
+  FINISH_STROKE,
+  REDO,
+  SET_STROKE_COLOR,
+  START_STROKE,
+  UNDO,
+  UPDATE_STROKE,
+} from './actions'
 import { RootState } from './types'
 
 const initialState: RootState = {
   currentStroke: { points: [], color: '#000' },
   strokes: [],
+  historyIndex: 0,
 }
 
-export const rootReducer: Reducer<RootState, Action> = (
-  state = initialState,
-  action
+export const rootReducer = (
+  state: RootState = initialState,
+  action: Action
 ) => {
   switch (action.type) {
     case START_STROKE: {
@@ -31,16 +38,41 @@ export const rootReducer: Reducer<RootState, Action> = (
         },
       }
     }
+    case SET_STROKE_COLOR: {
+      return {
+        ...state,
+        currentStroke: {
+          ...state.currentStroke,
+          ...{ color: action.payload },
+        },
+      }
+    }
     case FINISH_STROKE: {
       if (!state.currentStroke.points.length) {
         return state
       }
 
+      const historyIndex = state.strokes.length - state.historyIndex
+
       return {
         ...state,
+        historyIndex: 0,
         currentStroke: { ...state.currentStroke, points: [] },
-        strokes: [...state.strokes, state.currentStroke],
+        strokes: [...state.strokes.slice(0, historyIndex), state.currentStroke],
       }
+    }
+    case UNDO: {
+      const historyIndex = Math.min(
+        state.historyIndex + 1,
+        state.strokes.length
+      )
+
+      return { ...state, historyIndex }
+    }
+    case REDO: {
+      const historyIndex = Math.max(state.historyIndex - 1, 0)
+
+      return { ...state, historyIndex }
     }
     default:
       return state
@@ -48,3 +80,5 @@ export const rootReducer: Reducer<RootState, Action> = (
 }
 
 export const currentStrokeSelector = (state: RootState) => state.currentStroke
+export const historyIndexSelector = (state: RootState) => state.historyIndex
+export const strokesSelector = (state: RootState) => state.strokes
