1f75964e29b8646c2ded883951d4526f306fba7c
Jp Sia <johnpaulsia4@gmail.com>
2023-07-07 14:09:05 +0800

    chore: update actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -2,7 +2,10 @@ import { Point } from './types'
 
 export const START_STROKE = 'START_STROKE'
 export const UPDATE_STROKE = 'UPDATE_STROKE'
+export const SET_STROKE_COLOR = 'SET_STROKE_COLOR'
 export const FINISH_STROKE = 'FINISH_STROKE'
+export const UNDO = 'UNDO'
+export const REDO = 'REDO'
 
 export type Action =
   | {
@@ -14,7 +17,17 @@ export type Action =
       payload: Point
     }
   | {
+      type: typeof SET_STROKE_COLOR
+      payload: string
+    }
+  | {
       type: typeof FINISH_STROKE
+    }
+  | {
+      type: typeof UNDO
+    }
+  | {
+      type: typeof REDO
     }
 
 export const startStroke = (x: number, y: number) => {
@@ -25,6 +38,18 @@ export const updateStroke = (x: number, y: number) => {
   return { type: UPDATE_STROKE, payload: { x, y } }
 }
 
+export const setStrokeColor = (color: string) => {
+  return { type: SET_STROKE_COLOR, payload: color }
+}
+
 export const finishStroke = () => {
   return { type: FINISH_STROKE }
+}
+
+export const undo = () => {
+  return { type: UNDO }
+}
+
+export const redo = () => {
+  return { type: REDO }
 }
