ac242e012b6b81086a10de2ab6aba9974f3133f7
Jp Sia <johnpaulsia4@gmail.com>
2023-07-07 14:06:30 +0800

    feat: add edit panel and color panel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Canvas from '@/components/canvas'
+import ColorPanel from '@/components/color-panel'
 import TitleBar from '@/components/title-bar'
+import EditPanel from './components/edit-panel'
 
 function App() {
   return (
     <main className='window'>
       <TitleBar />
+      <EditPanel />
+      <ColorPanel />
       <Canvas />
     </main>
   )
